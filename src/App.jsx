17fbd0f41805254f50fbbd532445dbbd8745a0c3
Aroyan Bakti Setiawan <abssneakers@gmail.com>
2022-03-02 18:57:57 +0700

    Delete unsued components import

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
-import { ChakraProvider, Container, Heading } from '@chakra-ui/react';
-// import Calculator from './Calculator';
-// import Free from './Free';
-// import Kalkulator from './Kalkulator';
+import { ChakraProvider, Container } from "@chakra-ui/react";
+import Kalkulator from "./Kalkulator";
 
 function App() {
   return (
     <ChakraProvider>
       <Container
         className="App"
-        minW={'full'}
-        minH={'100vh'}
-        bg={'black'}
-        color={'white'}
+        minW={"full"}
+        minH={"100vh"}
+        bg={"black"}
+        color={"white"}
       >
-        <Heading as={'h1'} textAlign={'center'} p={'10'}>
-          Calculator
-        </Heading>
-        {/* <Calculator /> */}
-        {/* <Kalkulator /> */}
-        {/* <Free /> */}
+        <Kalkulator />
       </Container>
     </ChakraProvider>
   );
