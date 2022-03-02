1c506f3708e396e99e41e5a9bc416e38e8982fc1
Aroyan Bakti Setiawan <abssneakers@gmail.com>
2022-03-02 18:58:21 +0700

    Still working on logic of state

--- src/Kalkulator.jsx
+++ src/Kalkulator.jsx
@@ -1,42 +1,126 @@
-import React, { useState } from 'react';
+import { Box, Button, Heading } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+const endWithOperators = /[-=/+*]$/;
 
 export default function Kalkulator() {
-  const [currentVal, setCurrentVal] = useState('0');
-  const [prevVal, setPrevVal] = useState('0');
-  const [formula, setFormula] = useState('');
-  const [currentSign, setCurrentSign] = useState('pos');
-  const [lastClicked, setLastClicked] = useState('');
+  const [currentVal, setCurrentVal] = useState("0");
+  const [prevVal, setPrevVal] = useState("0");
+  const [formula, setFormula] = useState("");
+  const [currentSign, setCurrentSign] = useState("pos");
+  const [lastClicked, setLastClicked] = useState("");
   const [evaluated, setEvaluated] = useState(false);
 
-  function handleOperators() {
-    //code
+  function handleOperators(e) {
+    if (prevVal[prevVal.length - 1].match(endWithOperators)) {
+      setPrevVal(prevVal.replace(endWithOperators, e.target.value));
+    } else {
+      setPrevVal(prevVal + e.target.value);
+      setFormula(e.target.value);
+    }
   }
 
   function handleEvaluate() {
-    //code
+    setEvaluated(eval(prevVal));
+    setPrevVal("");
   }
 
   function handleDecimal() {
     //code
   }
 
-  function handleNumbers() {
-    //code
+  function handleNumbers(e) {
+    if (prevVal.length <= 1 && e.target.value === "0") {
+      setPrevVal(prevVal);
+    } else if (prevVal[0] === "0") {
+      setPrevVal(prevVal.substring(1) + e.target.value);
+    } else {
+      setPrevVal(prevVal + e.target.value);
+      setCurrentVal(e.target.value);
+    }
   }
 
   // AC Button
   function initialize() {
-    setCurrentVal('0');
-    setPrevVal('0');
-    setFormula('');
-    setCurrentSign('pos');
-    setLastClicked('');
+    setCurrentVal("0");
+    setPrevVal("0");
+    setFormula("");
+    setCurrentSign("pos");
+    setLastClicked("");
     setEvaluated(false);
   }
 
   return (
-    <div>
-      <h1>Setup</h1>
-    </div>
+    <Box color={"black"} id={"display"}>
+      <Heading as={"h1"} color={"whatsapp.500"} mb={"5"}>
+        Setup
+      </Heading>
+      <Heading as={"h2"} color={"white"} mb={"4"}>
+        {currentVal}
+      </Heading>
+      <Heading as={"h2"} color={"orange"} mb={"4"}>
+        {formula}
+      </Heading>
+      <Heading as={"h2"} color={"yellow"} mb={"4"}>
+        {prevVal}
+      </Heading>
+      <Heading as={"h2"} color={"green"} mb={"4"}>
+        {evaluated}
+      </Heading>
+      <Button onClick={handleNumbers} value={"1"} id={"one"}>
+        1
+      </Button>
+      <Button onClick={handleNumbers} value={"2"} id={"two"}>
+        2
+      </Button>
+      <Button onClick={handleNumbers} value={"3"} id={"three"}>
+        3
+      </Button>
+      <Button onClick={handleNumbers} value={"4"} id={"four"}>
+        4
+      </Button>
+      <Button onClick={handleNumbers} value={"5"} id={"five"}>
+        5
+      </Button>
+      <Button onClick={handleNumbers} value={"6"} id={"six"}>
+        6
+      </Button>
+      <Button onClick={handleNumbers} value={"7"} id={"seven"}>
+        7
+      </Button>
+      <Button onClick={handleNumbers} value={"8"} id={"eight"}>
+        8
+      </Button>
+      <Button onClick={handleNumbers} value={"9"} id={"nine"}>
+        9
+      </Button>
+      <Button onClick={handleNumbers} value={"0"} id={"zero"}>
+        0
+      </Button>
+      <Button onClick={handleNumbers} value={"."} id={"decimal"}>
+        .
+      </Button>
+      <br />
+      <Box mt={"4"} color={"purple"}>
+        <Button onClick={handleOperators} value={"+"} id={"add"}>
+          +
+        </Button>
+        <Button onClick={handleOperators} value={"-"} id={"subtract"}>
+          -
+        </Button>
+        <Button onClick={handleOperators} value={"*"} id={"multiply"}>
+          x
+        </Button>
+        <Button onClick={handleOperators} value={"/"} id={"divide"}>
+          /
+        </Button>
+        <Button onClick={handleEvaluate} value={"="} id={"equals"}>
+          =
+        </Button>
+        <Button onClick={initialize} value={"AC"} id={"clear"}>
+          AC
+        </Button>
+      </Box>
+    </Box>
   );
 }
